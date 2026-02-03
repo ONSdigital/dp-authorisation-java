39890fa094a18a893bf42031efbdb63a59d84cc7
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2026-02-03 08:55:35 +0000

    Reformat permissions checker

--- src/main/java/com/github/onsdigital/dp/authorisation/permissions/PermissionChecker.java
+++ src/main/java/com/github/onsdigital/dp/authorisation/permissions/PermissionChecker.java
@@ -25,15 +25,15 @@ public class PermissionChecker {
     }
 
     /**
-    * PermissionChecker.
-    *
-    * @param permissionsAPIHost  - permissionsAPIHost
-    * @param cacheUpdateInterval - cacheUpdateInterval
-    * @param expiryCheckInterval - expiryCheckInterval
-    * @param maxCacheTime        - maxCacheTime
-    */
+     * PermissionChecker.
+     *
+     * @param permissionsAPIHost  - permissionsAPIHost
+     * @param cacheUpdateInterval - cacheUpdateInterval
+     * @param expiryCheckInterval - expiryCheckInterval
+     * @param maxCacheTime        - maxCacheTime
+     */
     public PermissionChecker(String permissionsAPIHost, Duration cacheUpdateInterval,
-        Duration expiryCheckInterval, Duration maxCacheTime) {
+            Duration expiryCheckInterval, Duration maxCacheTime) {
         CachingStore cachingStore = new CachingStore(new APIClient(permissionsAPIHost));
         cachingStore.startCacheUpdater(cacheUpdateInterval);
         cachingStore.startExpiryChecker(expiryCheckInterval, maxCacheTime);
@@ -51,24 +51,29 @@ public class PermissionChecker {
     }
 
     /**
-     * hasPermission returns true if one of the given entities has the given permission.
+     * hasPermission returns true if one of the given entities has the given
+     * permission.
      *
-     * @param userData   - ID of the caller (user or service), as well as any associated groups
-     * @param permission - the action or permission the user wants to take, e.g. `datasets:edit`
-     * @param attributes - other key value attributes for use in access control decision,
-     *                   e.g. `collectionID`, `datasetID`, `isPublished`, `roleId`, etc
+     * @param userData   - ID of the caller (user or service), as well as any
+     *                   associated groups
+     * @param permission - the action or permission the user wants to take, e.g.
+     *                   `datasets:edit`
+     * @param attributes - other key value attributes for use in access control
+     *                   decision,
+     *                   e.g. `collectionID`, `datasetID`, `isPublished`, `roleId`,
+     *                   etc
      * @return boolean - authorisation status
      * @throws Exception -
      */
     public Boolean hasPermission(UserDataPayload userData,
-                                 String permission, Map<String, String> attributes) throws Exception {
+            String permission, Map<String, String> attributes) throws Exception {
         List<String> entities = mapEntityDataToEntities(userData);
         return hasPermission(entities, permission, attributes);
     }
 
     private Boolean hasPermission(List<String> entities,
-                                  String permission,
-                                  Map<String, String> attributes) throws Exception {
+            String permission,
+            Map<String, String> attributes) throws Exception {
 
         Bundle permissionsBundle = cache.getPermissionsBundle();
 
